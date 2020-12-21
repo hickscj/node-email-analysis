c50ef9cfd984a0ec60c9fdd9d9a6e285a2af09ee
Chad Hicks <chadjhicks@gmail.com>
2020-12-21 11:26:31 -0500

    initial commit

---
+++ emailAnalysis.js
@@ -0,0 +1,70 @@
+/* email analysis! */
+const Imap = require('imap');
+const dotenv = require('dotenv').config();
+const inspect = require('util').inspect;
+
+const getEmail = (server, address) => {
+    const imap = new Imap({
+        user: process.env.ADDR,
+        password: process.env.PASS,
+        host: server,
+        port: 993,
+        tls: true,
+        // debug: (debug) => {
+        //     console.log(debug);
+        // }
+    });
+
+    function openInbox(cb) {
+        imap.openBox('trump', true, cb);
+    }
+    
+    imap.once('ready', function() {
+        openInbox(function(err, box) {
+            if (err) throw err;
+            var f = imap.seq.fetch('1:3', {
+                bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+                struct: true
+            });
+            f.on('message', function(msg, seqno) {
+                console.log('Message #%d', seqno);
+                var prefix = '(#' + seqno + ') ';
+                msg.on('body', function(stream, info) {
+                    var buffer = '';
+                    stream.on('data', function(chunk) {
+                        buffer += chunk.toString('utf8');
+                    });
+                    stream.once('end', function() {
+                        console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+                    });
+                });
+                msg.once('attributes', function(attrs) {
+                    console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+                });
+                msg.once('end', function() {
+                    console.log(prefix + 'Finished');
+                });
+            });
+            f.once('error', function(err) {
+                console.log('Fetch error: ' + err);
+            });
+            f.once('end', function() {
+                console.log('Done fetching all messages!');
+                imap.end();
+            });
+        });
+    });
+    
+    imap.once('error', function(err) {
+        console.log(err);
+    });
+    
+    imap.once('end', function() {
+        console.log('Connection ended');
+    });
+    
+    imap.connect();
+
+};
+
+getEmail("imap.mail.yahoo.com", "chadjhicks@yahoo.com");
